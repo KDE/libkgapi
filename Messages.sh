623459689f3598b30177170f1c75dcbe01162b3d
Pino Toscano <pino@kde.org>
2011-06-14 15:05:29 +0200

    i18n: fix messages extraction
    - extract from .ui and .kcfg files
    - extract from any C++ source, excluding the ones in 'tests' subdirectories

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
-$XGETTEXT *.cpp -o $podir/akonadi_akonadi-google_resource.pot
+$EXTRACTRC `find . -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
+$XGETTEXT `find . -name \*.cpp | grep -v '/tests/'` -o $podir/akonadi_akonadi-google_resource.pot
+rm -f rc.cpp
