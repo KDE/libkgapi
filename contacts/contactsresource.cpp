KDE/libkgapi

commit 2450250ebb7132c3592f25dc0d0b69b6e8e80c05

Emit error when KGoogleAuth is unable to refresh token

contacts/contactsresource.cpp
@@ -67,7 +67,9 @@ ContactsResource::ContactsResource(const QString &id):
 			   Service::Addressbook::scopeUrl());
   connect(m_auth, SIGNAL(tokensRecevied(QString,QString)),
 	  this, SLOT(tokensReceived()));
-  
+  connect(m_gam, SIGNAL(authError(QString)),
+          this, SLOT(authError(QString)));
+
   m_gam = new KGoogleAccessManager(m_auth);
   
   m_photoNam = new KIO::Integration::AccessManager(this);
@@ -114,6 +116,14 @@ void ContactsResource::slotGAMError(const QString &msg, const int errorCode)
   cancelTask(msg);
 
   Q_UNUSED(errorCode);
+}
+
+void ContactsResource::slotAuthError(const QString &error)
+{
+  cancelTask(error);
+
+  setOnline(false);
+  emit status(Broken, error);
 }
 
 void ContactsResource::configure(WId windowId)

calendar/calendarresource.cpp
@@ -79,6 +79,8 @@ CalendarResource::CalendarResource(const QString &id):
 	  this, SLOT(replyReceived(KGoogleReply*)));
   connect(m_gam, SIGNAL(requestFinished(KGoogleRequest*)),
 	  this, SLOT(commitItemsList()));
+  connect(m_gam, SLOT(authError(QString)),
+          this, SLOT(authError(QString)));
 
   connect(this, SIGNAL(abortRequested()),
 	  this, SLOT(slotAbortRequested()));
@@ -113,6 +115,14 @@ void CalendarResource::slotGAMError(const QString &msg, const int errorCode)
   cancelTask(msg);
 
   Q_UNUSED(errorCode);
+}
+
+void CalendarResource::authError (const QString &error)
+{
+  cancelTask(error);
+
+  setOnline(false);
+  emit status(Broken, error);
 }
 
 

calendar/calendarresource.h
@@ -58,6 +58,7 @@ class CalendarResource : public Akonadi::ResourceBase, public Akonadi::AgentBase
 
   private Q_SLOTS:
     void slotGAMError(const QString &msg, const int errorCode);
+    void authError(const QString &error);
     void slotAbortRequested();
     void tokensReceived();
 

calendar/settingsdialog.cpp
@@ -111,6 +111,7 @@ void SettingsDialog::setAuthenticated(bool authenticated)
   m_ui->revokeAuthLabel->setVisible(authenticated);
   
   m_ui->calendarGroupBox->setEnabled(authenticated);
+  m_ui->comboBox->setEnabled(authenticated);
 }
 
 

contacts/contactsresource.h
@@ -116,6 +116,7 @@ class ContactsResource: public Akonadi::ResourceBase,
 
   private Q_SLOTS:
     void slotGAMError(const QString &msg, const int errorCode);
+    void slotAuthError(const QString &msg);
     void slotAbortRequested();
     void tokensReceived();
     void initialItemFetchJobFinished(KJob *job);

libkgoogle/kgoogleaccessmanager.cpp
@@ -56,6 +56,8 @@ KGoogleAccessManager::KGoogleAccessManager(KGoogle::KGoogleAuth *googleAuth):
 
   connect(m_auth, SIGNAL(tokensRecevied(QString,QString)),
 	  this, SLOT(newTokensReceived()));
+  connect(m_auth, SIGNAL(error(QString)),
+          this, SIGNAL(authError(QString)));
 }
 
 KGoogleAccessManager::~KGoogleAccessManager()

libkgoogle/kgoogleaccessmanager.h
@@ -116,6 +116,11 @@ namespace KGoogle {
        */
       void error(const QString &msg, const int errorCode);
 
+      /**
+       * An authentication-related error occured.
+       */
+      void authError(const QString &msg);
+
     private Q_SLOTS:
       void nam_replyReceived(QNetworkReply *reply);
       void nam_sendRequest(KGoogleRequest *request);

libkgoogle/kgoogleauth.cpp
@@ -79,22 +79,31 @@ void KGoogleAuth::refreshToken()
   QNetworkAccessManager *nam = new KIO::Integration::AccessManager(this);
   QNetworkRequest request;
   QByteArray data;
-  
+
+  if (m_refreshToken.isEmpty()) {
+    emit error("Failed to refresh token. Please reauthenticate");
+    return;
+  }
+
   connect(nam, SIGNAL(finished(QNetworkReply*)),
 	  this, SLOT(refreshTokenFinished(QNetworkReply*)));
   connect(nam, SIGNAL(finished(QNetworkReply*)),
 	  nam, SLOT(deleteLater()));
   
-  kDebug() << "requesting token refresh";
-  
   request.setUrl(QUrl("https://accounts.google.com/o/oauth2/token"));
+  request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+
+  QUrl params;
+  params.addQueryItem("client_id", m_clientId);
+  params.addQueryItem("client_secret", m_clientSecret);
+  params.addQueryItem("refresh_token", m_refreshToken);
+  params.addQueryItem("grant_type", "refresh_token");
+
+#ifdef DEBUG_RAWDATA
+  kDebug() << "Requesting token refresh: " << params.encodedQuery();
+#endif
   
-  data = QString("client_id=" + m_clientId + "&" +
-	 "client_secret=" + m_clientSecret + "&" +
-	 "refresh_token=" + m_refreshToken + "&" +
-	 "grant_type=refresh_token").toLatin1();
-  
-  nam->post(request, data);
+  nam->post(request, params.encodedQuery());
   
 }
 
@@ -126,6 +135,10 @@ void KGoogleAuth::refreshTokenFinished(QNetworkReply* reply)
   
   QByteArray data = reply->readAll();
   QJson::Parser parser;
+  
+#ifdef DEBUG_RAWDATA
+  kDebug() << "Tokens refreshed: " << data;
+#endif
   
   /* Expected structure: 
    * {

commit 6533f503a7e9fad3ab377777b135ee4a40b8c6d5

Fix refreshing of token in Contacts resource (bug #288829)
Fix couple of bugs introduced in commit 2450250ebb7132c3592f25dc0d0b69b6e8e80c05

contacts/contactsresource.cpp
@@ -67,11 +67,12 @@ ContactsResource::ContactsResource(const QString &id):
 			   Service::Addressbook::scopeUrl());
   connect(m_auth, SIGNAL(tokensRecevied(QString,QString)),
 	  this, SLOT(tokensReceived()));
-  connect(m_gam, SIGNAL(authError(QString)),
-          this, SLOT(authError(QString)));
 
   m_gam = new KGoogleAccessManager(m_auth);
-  
+
+  connect(m_gam, SIGNAL(authError(QString)),
+          this, SLOT(slotAuthError(QString)));
+
   m_photoNam = new KIO::Integration::AccessManager(this);
   
   connect(m_gam, SIGNAL(replyReceived(KGoogleReply*)),