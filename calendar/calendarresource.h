02adef973e2eed9ebfc821f574144e1395416304
Dan Vrátil <dan@progdan.cz>
2012-02-22 23:31:11 +0100

    Minor coding style fix

--- calendar/calendarresource.h
+++ calendar/calendarresource.h
@@ -91,7 +91,7 @@ class CalendarResource : public Akonadi::ResourceBase, public Akonadi::AgentBase
     void eventUpdated(KGoogle::Reply *reply);
     void eventRemoved(KGoogle::Reply *reply);
 
-    void emitPercent(KJob*job, ulong percent);
+    void emitPercent(KJob* job, ulong percent);
 
   private:
     void abort();
