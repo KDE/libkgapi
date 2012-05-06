df8982ece529b87d3f801b6ab8ae3d73cbbd2f1a
Montel Laurent <montel@kde.org>
2012-05-06 11:35:54 +0200

    Minor

--- libkgoogle/libkgoogle_export.h
+++ libkgoogle/libkgoogle_export.h
@@ -1,7 +1,12 @@
+#ifndef LIBKGOOGLE_EXPORT_H
+#define LIBKGOOGLE_EXPORT_H
+
 #include <QtGlobal>
 
 #if defined(LIBKGOOGLE_LIBRARY)
 #  define LIBKGOOGLE_EXPORT Q_DECL_EXPORT
 #else
 #  define LIBKGOOGLE_EXPORT Q_DECL_IMPORT
+#endif
+
 #endif
