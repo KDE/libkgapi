2b177aba36007c8171b2661ee459c391ba04779e
Dan Vrátil <dan@progdan.cz>
2012-05-24 19:50:13 +0200

    Rename catalog file to libkgoogle.pot

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp | grep -v '/tests/'` -o $podir/akonadi_akonadi-google_resource.pot
+$XGETTEXT `find . -name \*.cpp | grep -v '/tests/'` -o $podir/libkgoogle.pot
 rm -f rc.cpp
