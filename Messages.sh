b959465629f5ff6eb0b982bf31757945ed4e1010
Montel Laurent <montel@kde.org>
2012-06-04 15:51:57 +0200

    Fix extract i18n

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp | grep -v '/tests/'` -o $podir/libkgapi.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/libkgapi.pot
 rm -f rc.cpp
