bcea583765551c7dd045bdb3f70a8476a3ee9b7a
Pino Toscano <pino@kde.org>
2012-08-10 09:11:02 +0200

    i18n: extract messages only in the 'libkgapi' subdirectory
    ... as only those matter, the rest are uninstalled examples.
    remove also the extraction of .ui and .kcfg files, since there are none now

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/libkgapi.pot
+$XGETTEXT `find libkgapi -name \*.cpp -o -name \*.h` -o $podir/libkgapi.pot
 rm -f rc.cpp
