4a9b84dabc376242bb24cfd2128b67d72588ad46
Montel Laurent <montel@kde.org>
2014-02-06 13:32:22 +0100

    normalize signal/slot

--- libkgapi2/drive/fileabstractuploadjob.cpp
+++ libkgapi2/drive/fileabstractuploadjob.cpp
@@ -300,8 +300,8 @@ void FileAbstractUploadJob::dispatchRequest(QNetworkAccessManager *accessManager
 
     QNetworkReply *reply = dispatch(accessManager, request, data);
 
-    connect(reply, SIGNAL(uploadProgress(qint64, qint64)),
-            this, SLOT(_k_uploadProgress(qint64, qint64)));
+    connect(reply, SIGNAL(uploadProgress(qint64,qint64)),
+            this, SLOT(_k_uploadProgress(qint64,qint64)));
 }
 
 void FileAbstractUploadJob::handleReply(const QNetworkReply *reply,
