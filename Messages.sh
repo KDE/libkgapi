64333c7d882af859029d2f35fae5fe0da558996a
Albert Astals Cid <aacid@kde.org>
2015-07-30 16:37:30 +0200

    libkgapi is now using tr()

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find src -name \*.cpp -o -name \*.h` -o $podir/libkgapi.pot
-rm -f rc.cpp
+$EXTRACT_TR_STRINGS `find src -name \*.cpp -o -name \*.h` -o $podir/libkgapi_qt.pot
