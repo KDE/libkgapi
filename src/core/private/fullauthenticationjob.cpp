KDE/libkgapi

commit b9fe0d8ea6edf29e0de4296f81ba8e27c8616c3d

Modernize code

src/core/private/fullauthenticationjob.cpp
@@ -14,12 +14,13 @@
 #include "../../debug.h"
 
 #include <QAbstractSocket>
+#include <QDateTime>
+#include <QDesktopServices>
 #include <QTcpServer>
 #include <QTcpSocket>
-#include <QDesktopServices>
-#include <QUrlQuery>
 #include <QUrl>
-#include <QDateTime>
+#include <QUrlQuery>
+#include <memory>
 
 using namespace KGAPI2;
 
@@ -190,7 +191,7 @@ void FullAuthenticationJob::start()
         scopes << scope.toString();
     }
 
-    d->mServer = std::unique_ptr<QTcpServer>(new QTcpServer(this));
+    d->mServer = std::make_unique<QTcpServer>(this);
     if (!d->mServer->listen(QHostAddress::LocalHost, d->mServerPort)) {
         d->emitError(InvalidAccount, tr("Could not start OAuth HTTP server"));
         return;

autotests/drive/drivescreatejobtest.cpp
@@ -57,7 +57,7 @@ private Q_SLOTS:
         FakeNetworkAccessManagerFactory::get()->setScenarios(scenarios);
 
         auto account = AccountPtr::create(QStringLiteral("MockAccount"), QStringLiteral("MockToken"));
-        Drive::DrivesCreateJob *job = new Drive::DrivesCreateJob(requestId, sourceDrives, account);
+        auto *job = new Drive::DrivesCreateJob(requestId, sourceDrives, account);
 
         QVERIFY(execJob(job));
         const auto items = job->items();

autotests/drive/drivesdeletejobtest.cpp
@@ -50,7 +50,7 @@ private Q_SLOTS:
         FakeNetworkAccessManagerFactory::get()->setScenarios(scenarios);
 
         auto account = AccountPtr::create(QStringLiteral("MockAccount"), QStringLiteral("MockToken"));
-        Drive::DrivesDeleteJob *job = new Drive::DrivesDeleteJob(drivesId, account);
+        auto *job = new Drive::DrivesDeleteJob(drivesId, account);
 
         QVERIFY(execJob(job));
     }

autotests/drive/driveshidejobtest.cpp
@@ -54,7 +54,7 @@ private Q_SLOTS:
         FakeNetworkAccessManagerFactory::get()->setScenarios(scenarios);
 
         auto account = AccountPtr::create(QStringLiteral("MockAccount"), QStringLiteral("MockToken"));
-        Drive::DrivesHideJob *job = new Drive::DrivesHideJob(sourceDrive, true, account);
+        auto *job = new Drive::DrivesHideJob(sourceDrive, true, account);
 
         QVERIFY(execJob(job));
         const auto items = job->items();

autotests/drive/drivesmodifyjobtest.cpp
@@ -56,7 +56,7 @@ private Q_SLOTS:
         sourceDrives->setName(QStringLiteral("Renamed Drive"));
 
         auto account = AccountPtr::create(QStringLiteral("MockAccount"), QStringLiteral("MockToken"));
-        Drive::DrivesModifyJob *job = new Drive::DrivesModifyJob(sourceDrives, account);
+        auto *job = new Drive::DrivesModifyJob(sourceDrives, account);
 
         QVERIFY(execJob(job));
 

autotests/drive/teamdrivecreatejobtest.cpp
@@ -57,7 +57,7 @@ private Q_SLOTS:
         FakeNetworkAccessManagerFactory::get()->setScenarios(scenarios);
 
         auto account = AccountPtr::create(QStringLiteral("MockAccount"), QStringLiteral("MockToken"));
-        Drive::TeamdriveCreateJob *job = new Drive::TeamdriveCreateJob(requestId, sourceTeamdrive, account);
+        auto *job = new Drive::TeamdriveCreateJob(requestId, sourceTeamdrive, account);
 
         QVERIFY(execJob(job));
         const auto items = job->items();

autotests/drive/teamdrivedeletejobtest.cpp
@@ -50,7 +50,7 @@ private Q_SLOTS:
         FakeNetworkAccessManagerFactory::get()->setScenarios(scenarios);
 
         auto account = AccountPtr::create(QStringLiteral("MockAccount"), QStringLiteral("MockToken"));
-        Drive::TeamdriveDeleteJob *job = new Drive::TeamdriveDeleteJob(teamdriveId, account);
+        auto *job = new Drive::TeamdriveDeleteJob(teamdriveId, account);
 
         QVERIFY(execJob(job));
     }

autotests/drive/teamdrivemodifyjobtest.cpp
@@ -56,7 +56,7 @@ private Q_SLOTS:
         sourceTeamdrive->setName(QStringLiteral("Renamed Team Drive"));
 
         auto account = AccountPtr::create(QStringLiteral("MockAccount"), QStringLiteral("MockToken"));
-        Drive::TeamdriveModifyJob *job = new Drive::TeamdriveModifyJob(sourceTeamdrive, account);
+        auto *job = new Drive::TeamdriveModifyJob(sourceTeamdrive, account);
 
         QVERIFY(execJob(job));
 

commit 8eb8a5e963c62c09f988f67e3fe0532db039b63d

Fix crash when deleting FullAuthenticationJob
Regression introduced in b9fe0d8e, which made QTcpServer a std::unique_ptr,
but forgot to remove a deleteLater() statement few lines below.

src/core/private/fullauthenticationjob.cpp
@@ -192,7 +192,7 @@ void FullAuthenticationJob::start()
         scopes << scope.toString();
     }
 
-    d->mServer = std::make_unique<QTcpServer>(this);
+    d->mServer = std::make_unique<QTcpServer>();
     if (!d->mServer->listen(QHostAddress::LocalHost, d->mServerPort)) {
         d->emitError(InvalidAccount, tr("Could not start OAuth HTTP server"));
         return;
@@ -206,7 +206,6 @@ void FullAuthenticationJob::start()
                 this, [this](QAbstractSocket::SocketError e) { d->socketError(e); });
         connect(d->mConnection, &QTcpSocket::readyRead, this, [this]() { d->socketReady(); });
         d->mServer->close();
-        d->mServer->deleteLater();
     });
 
     QUrl url(QStringLiteral("https://accounts.google.com/o/oauth2/auth"));