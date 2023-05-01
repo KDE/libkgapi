352a5fa2a881199c73b3a6e02f212aad4796efbf
Carl Schwan <carl@carlschwan.eu>
2023-05-01 11:08:59 +0200

    Fix copy paste error in license

--- autotests/people/contactgroupmodifyjobtest.cpp
+++ autotests/people/contactgroupmodifyjobtest.cpp
@@ -1,7 +1,7 @@
 /*
  * SPDX-FileCopyrightText: 2023 Claudio Cambra <claudio.cambra@kde.org>
  *
- * SPDX-License-Identifier: LGPL-2.2-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+ * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
  */
 
 #include <QObject>
