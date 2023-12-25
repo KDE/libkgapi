695e835ab1e7685ef708360473145c783c2f42f4
Laurent Montel <montel@kde.org>
2023-12-25 19:56:34 +0100

    Use libkgapi6_qt

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACT_TR_STRINGS `find src -name \*.cpp -o -name \*.h` -o $podir/libkgapi_qt6.pot
+$EXTRACT_TR_STRINGS `find src -name \*.cpp -o -name \*.h` -o $podir/libkgapi6_qt.pot
